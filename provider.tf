35f91e073028afa371231696825fd32d9b4b7afa
mohitarmal <83169868+mohitarmal@users.noreply.github.com>
2021-04-25 19:15:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,21 +5,20 @@ provider "aws" {
   version = ">= 2.38.0"
 }
 
-resource "aws_vpc" "demo" {
+resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "demo"
-        Location = "Pune"
+    Name = "main"
   }
 }
 
-resource "aws_subnet" "demopublic" {
+resource "aws_subnet" "mainpublic" {
   count = 2
 
   cidr_block        = "10.0.6${count.index}.0/24"
-  vpc_id            = aws_vpc.demo.id
+  vpc_id            = aws_vpc.main.id
   map_public_ip_on_launch = "true"
 
   tags = {
@@ -27,12 +26,12 @@ resource "aws_subnet" "demopublic" {
     }
 }
 
-resource "aws_subnet" "demo" {
+resource "aws_subnet" "mainprivate" {
   count = 2
 
   #availability_zone = data.aws_availability_zones.available.names[count.index]
   cidr_block        = "10.0.${count.index}.0/24"
-  vpc_id            = aws_vpc.demo.id
+  vpc_id            = aws_vpc.main.id
 
   tags = {
     Name = "PrivateSubnet${count.index}",
